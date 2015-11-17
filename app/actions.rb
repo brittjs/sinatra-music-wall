c2526d4ec2a6e284d52c831d9615070e11405e68
Brittany See <brittanyjsee@gmail.com>
2015-11-17 02:03:54 +0000

    submitting a track

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,18 @@ get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
 end
+
+get '/tracks/new' do
+  erb :'tracks/new'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]  
+  )
+  @track.save
+  redirect '/tracks'
+end
+

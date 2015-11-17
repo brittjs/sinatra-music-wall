f0f89d8ff78242860d4331b5b06e1c185e8c3d86
Brittany See <brittanyjsee@gmail.com>
2015-11-17 02:27:50 +0000

    track detail page

--- app/actions.rb
+++ app/actions.rb
@@ -12,13 +12,20 @@ get '/tracks/new' do
   erb :'tracks/new'
 end
 
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
 post '/tracks' do
   @track = Track.new(
     song_title: params[:song_title],
     author: params[:author],
     url: params[:url]  
   )
-  @track.save
-  redirect '/tracks'
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'  
+  end
 end
-
